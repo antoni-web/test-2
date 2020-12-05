8c97ac3e3e468b660c950ccf30f838b18e9e87a4
antoni-web <antonisaputra350@gmail.com>
2020-12-05 16:19:05 +0700

    Menambahkan Ul

--- index.php
+++ index.php
@@ -10,5 +10,10 @@
     <h1>Hello Word!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum tempora impedit, in repellat ad inventore quod veniam animi atque nihil voluptatum delectus quibusdam maxime, magni rerum? Praesentium quidem sequi blanditiis!</p>
 
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
+
 </body>
 </html>
